b9df648ffe7bd4757883d7f6ab0719374a7d1bcc
Shafiq Salehi <rasashafiq@gmail.com>
2024-02-18 17:04:14 +0100

    create and update events

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as NavItems } from "./NavItems";
 export { default as MobileNav } from "./MobileNav";
+export { default as EventForm } from "./EventForm";
